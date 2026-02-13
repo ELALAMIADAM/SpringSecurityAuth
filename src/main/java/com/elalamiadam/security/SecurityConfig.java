51e1cff15c50a2581d6986b7dd8651b1e46f50cb
ELALAMIADAM <adamelalami27@gmail.com>
2026-02-13 23:18:13 +0100

    Refactor DBUser entity to include Lombok annotations for cleaner code. Update SecurityConfig to integrate CustomUserDetailsService and add an AuthenticationManager bean. Remove unused DBUserService class.

--- src/main/java/com/elalamiadam/security/SecurityConfig.java
+++ src/main/java/com/elalamiadam/security/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.elalamiadam.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,9 +15,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.elalamiadam.service.CustomUserDetailsService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
     
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity ) throws Exception{
@@ -39,4 +46,10 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+        return authenticationManagerBuilder.build();
+    }
 }
--- src/main/java/com/elalamiadam/service/DBUserService.java
+++
@@ -1,8 +0,0 @@
-package com.elalamiadam.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class DBUserService {
-    
-}
