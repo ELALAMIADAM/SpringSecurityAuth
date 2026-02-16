38804375f5f44d91bdd0a655d4042cfcff38e504
ELALAMIADAM <ELALAMIADAM@users.noreply.github.com>
2026-02-16 12:04:47 +0100

    Enhance SpringSecurityAuthApplicationTests by adding a test for LoginController to ensure it is properly initialized. This includes importing necessary assertions and updating the contextLoads test method.

--- src/test/java/com/elalamiadam/SpringSecurityAuthApplicationTests.java
+++ src/test/java/com/elalamiadam/SpringSecurityAuthApplicationTests.java
@@ -1,13 +1,23 @@
 package com.elalamiadam;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.elalamiadam.Controller.LoginController;
 
 @SpringBootTest
 class SpringSecurityAuthApplicationTests {
 
+	@Autowired
+	private LoginController loginController;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception{
+		assertThat(loginController).isNotNull();
 	}
 
 }
